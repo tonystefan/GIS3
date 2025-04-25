f21b96602d7874d08c54c8f48ab78acf36ad91ff
tonystefan <tonystefanlog@hotmail.com>
2025-04-25 13:06:13 -0300

    fist commit

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('home.urls')),
-    path('', include('carona.urls')),
-    path("admin/", admin.site.urls),
-    path('', include('admin_coreui.urls')),
-]
