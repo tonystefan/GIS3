81e3a9beaf276402d79e5d013a139be8f1bfb101
tonystefan <tonystefanlog@hotmail.com>
2025-04-27 18:31:11 -0300

    to_versel15

--- GIS/build_files.sh
+++ GIS/build_files.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
-set -o errexit
-
-python -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
