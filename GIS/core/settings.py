9d37b8e07e936c70d8a63afff0266459b08aff2f
tonystefan <tonystefanlog@hotmail.com>
2025-04-29 22:25:04 -0300

    forms7

--- GIS/core/settings.py
+++ GIS/core/settings.py
@@ -25,9 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    SECRET_KEY = ''.join(random.choice( string.ascii_lowercase  ) for i in range( 32 ))
+SECRET_KEY = config('SECRET_KEY')
 
 # Enable/Disable DEBUG Mode
 DEBUG = str2bool(os.environ.get('DEBUG'))
